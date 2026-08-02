92ed7d89915f3ba54d98b04be0fa0a1a37415d36
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-08-02 14:41:39 +0530

    Removed old backend

--- desktop/tests/test_socket.cpp
+++ desktop/tests/test_socket.cpp
@@ -1,12 +1,2 @@
 #include <gtest/gtest.h>
 #include "../source/connection/Socket.h"
-
-TEST(SocketTest, ConstructorCreateObject) {
-    Socket socket;
-    SUCCEED();
-}
-
-TEST(SocketInit, InitReturnsTrue) {
-    Socket socket;
-    EXPECT_TRUE(socket.webSocketInit());
-}
