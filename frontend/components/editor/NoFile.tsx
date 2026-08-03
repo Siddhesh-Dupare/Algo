03e50881b7484d2090bec5ea9fa95ff03c74aea1
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-08-03 18:26:26 +0530

    New frontend

--- frontend/components/editor/NoFile.tsx
+++
@@ -1,7 +0,0 @@
-export default function NoFile() {
-  return (
-    <div className="flex h-full items-center justify-center text-sm text-muted-foreground">
-      No file open
-    </div>
-  );
-}
--- frontend/components/explorer/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import FileTree from "./FileTree";
-
-export default function Sidebar() {
-  return (
-    <div className="h-full border-r border-border bg-muted/30">
-      <FileTree />
-    </div>
-  );
-}
